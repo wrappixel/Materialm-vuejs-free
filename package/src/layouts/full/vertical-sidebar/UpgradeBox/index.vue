6d57ce52e15f3f8182a9fac196f261ce33fefa3f
wrappixelTeam <114797723+wrappixelTeam@users.noreply.github.com>
2024-10-07 10:08:48 +0530

    ''

---
+++ package/src/layouts/full/vertical-sidebar/UpgradeBox/index.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <v-sheet rounded="md" color="lightprimary" class="position-relative extrabox hide-menu mx-1 px-4 py-3">
+        <div class="d-flex align-center">
+            <div>
+                <h6 class="text-h6 text-10 mb-3">Upgrade to pro</h6>
+                <v-btn target="_blank" href="https://www.wrappixel.com/templates/materialm-vuejs-vuetify-admin-template/?ref=33" color="primary" class="px-5" flat>Buy Pro</v-btn>
+            </div>
+            <div class="">
+                <img src="@/assets/images/backgrounds/upgrade.svg" height="100" width="100" class="px-2 ms-8" />
+            </div>
+        </div>
+    </v-sheet>
+</template>
+
